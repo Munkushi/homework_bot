9a0c422fedfd76b89716ba2bf4feefa519777064
Даниил Цыганов <daniil.tsiganov@gmail.com>
2022-02-01 22:07:20 +0300

    review

--- homework.py
+++ homework.py
@@ -20,7 +20,7 @@ PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-RETRY_TIME = 600
+TELEGRAM_RETRY_TIME = os.getenv("TELEGRAM_RETRY_TIME")
 ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
 HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 HOMEWORK_STATUSES = {
@@ -28,6 +28,33 @@ HOMEWORK_STATUSES = {
     "reviewing": "Работа взята на проверку ревьюером.",
     "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
+
+
+def logg():
+    """
+    функция логов.
+    """
+    logger = logging.getLogger(__name__)
+    handler = RotatingFileHandler(
+        "my_logger.log",
+        maxBytes=50000000,
+        backupCount=5
+    )
+    logger.addHandler(handler)
+
+
+class StatusError(Exception):
+    """
+    ошибка для прроверки статуса
+    """
+    pass
+
+
+class EmtpyHomeworkError(Exception):
+    """
+    список дз не пустой.
+    """
+    pass
 
 
 def send_message(bot, message):
@@ -41,20 +68,28 @@ def get_api_answer(current_timestamp):
     params = {"from_date": timestamp}
     response = requests.get(ENDPOINT, headers=HEADERS, params=params)
     if response.status_code != 200:
-        raise
+        logging.exception("Статус ответа не равен 200.")
+        raise StatusError("Статус ответа не равен 200.")
     return response.json()
 
 
 def check_response(response):
     """Ретернит список домашних работ."""
     homework = response["homeworks"]
+    if len(homework) == 0:
+        logging.exception("В ответе пустой список.")
+        raise EmtpyHomeworkError("Список дз пустой.")
     if type(homework) != list:
-        raise
-    return homework
+        logging.exception("Тип homework = list. Или список дз пустой.")
+        raise StatusError("Тип homework = list.")
+    else:
+        return homework
 
 
 def parse_status(homework):
     """Статус дз."""
+    if "status" not in homework.keys():
+        raise EmtpyHomeworkError("В словаре нет таких ключей")
     homework_name = homework["homework_name"]
     homework_status = homework["status"]
     verdict = HOMEWORK_STATUSES[homework_status]
@@ -63,48 +98,37 @@ def parse_status(homework):
 
 def check_tokens() -> bool:
     """Провека env-файла."""
-    check_file = os.path.exists(".env")
     if (
-            check_file is None
-            or PRACTICUM_TOKEN is None
+            PRACTICUM_TOKEN is None
             or TELEGRAM_TOKEN is None
             or TELEGRAM_CHAT_ID is None
     ):
+        logging.exception(f"Отстутствует переменная.")
         return False
-    else:
-        return True
+
+    return True
 
 
 def main():
     """Основная логика работы бота."""
-    logger = logging.getLogger(__name__)
-    handler = RotatingFileHandler(
-        "my_logger.log",
-        maxBytes=50000000,
-        backupCount=5
-    )
-    logger.addHandler(handler)
-
+    logg()
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     while True:
         try:
             response = get_api_answer(current_timestamp)
             logging.debug("Первый запрос к API.")
-            сheck_response_1 = response.get("homeworks")
-            if сheck_response_1:
+            new_hw = response.get("homeworks")
+            if new_hw:
                 logging.info("Работа найдена. Все хорошо.")
-                send_message(bot, parse_status(сheck_response_1[0]))
+                send_message(bot, parse_status(new_hw[0]))
             current_timestamp = response.get("current_date")
-            time.sleep(RETRY_TIME)
         except Exception as error:
             logging.exception(f"У бота случилась какая-то ошибка {error}")
-            message = f"Сбой в работе программы: {error}"
-            print(message)
-            time.sleep(RETRY_TIME)
-        else:
-            logging.error("Ошибка при запросе к основному API")
+        finally:
+            time.sleep(int(TELEGRAM_RETRY_TIME))
 
 
 if __name__ == "__main__":
-    main()
+    if check_tokens() is True:
+        main()
